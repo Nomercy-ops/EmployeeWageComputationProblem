34b293672b3e06bdb907ec834c6e64bcc2d87faa
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-26 15:51:47 +0530

    [Rikesh] Added: daily wage calculation for the part time employee

--- empWageComputation.py
+++ empWageComputation.py
@@ -13,9 +13,11 @@ def calculate_employee_wage(workRatePerHour):
     """
 
     Description:
-        This method is called for getting  employee daily wage,
+        This method is called for getting Fulltime employee 
+        daily wage and part time employee daily wages,
         By multiplying workrateperhour and wagerateperhour.
-        
+
+
     Parameter:
         workRatePerHour is will be 8 for full time employee and used for calculating daily wage.
 
@@ -23,18 +25,22 @@ def calculate_employee_wage(workRatePerHour):
 
     wageRatePerHour = 20
     employeeDailyWage = wageRatePerHour * workRatePerHour
-    print("Employee Daily wage is : " , employeeDailyWage ,"$")
+    print("Employee Daily wage is : ", employeeDailyWage, "$")
 
 
 if __name__ == "__main__":
-   
+
     print(" Welcome to Employee Wage Computation Program ")
-    attendance_check = random.randint(0, 1)
-    
+    attendance_check = random.randint(0, 2)
+
     # Taking fulltime as a constant variables
     IS_FULL_TIME = 1
+    IS_PART_TIME = 2
     if(attendance_check == IS_FULL_TIME):
         calculate_employee_wage(8)
+
+    elif(attendance_check == IS_PART_TIME):
+        calculate_employee_wage(4)
 
     else:
         calculate_employee_wage(0)
