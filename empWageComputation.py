1d07123b83ee44811f87aea7aacd2dec6350f39a
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-26 15:12:47 +0530

    [Rikesh] Added : random number generater between 0 to 1 and checking attendance of employee if number is 0 employee is present else absent

--- empWageComputation.py
+++ empWageComputation.py
@@ -2,12 +2,31 @@
 @Author: Rikesh Chhetri
 @Date: 2021-06-26 
 @Last Modified by: Rikesh Chhetri
-@Last Modified time: 2021-06-26 14:03:30
+@Last Modified time: 2021-06-26 15:02:30
 @Title : Program Aim is to Calculate the Employee Daily and Monthly  Wage.
 """
 
-def calculate_employee_wage():
-    
-    print(" Welcome to Employee Wage Computation Program ")
+import random
 
-calculate_employee_wage() 
+
+def calculate_employee_wage():
+    """
+
+    Description:
+        This method is called for getting attendance of an employee 
+        by genetaring random num between o and 1.
+        if number is 0 then employee is present else employee is absent.
+    
+    """
+
+    print(" Welcome to Employee Wage Computation Program ")
+    attendance_check = random.randint(0, 1)
+
+    if(attendance_check == 0):
+        print("Employee is Present")
+
+    else:
+        print("Employee is Absent")
+
+
+calculate_employee_wage()
