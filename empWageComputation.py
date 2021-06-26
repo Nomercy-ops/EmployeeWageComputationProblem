0f32d72ef33d31f166d3f23debdb681878a27ef4
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-26 14:58:23 +0530

    [Rikesh] Added: Display Message  Welcome to Employee Wage Computation Program on Master Branch

---
+++ empWageComputation.py
@@ -0,0 +1,13 @@
+"""
+@Author: Rikesh Chhetri
+@Date: 2021-06-26 
+@Last Modified by: Rikesh Chhetri
+@Last Modified time: 2021-06-26 14:03:30
+@Title : Program Aim is to Calculate the Employee Daily and Monthly  Wage.
+"""
+
+def calculate_employee_wage():
+    
+    print(" Welcome to Employee Wage Computation Program ")
+
+calculate_employee_wage() 
